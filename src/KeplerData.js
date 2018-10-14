6ae03adcdfad283756ec9a45136a39169bda01d2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-10-14 03:45:24 +0300

    .

--- src/KeplerData.js
+++ src/KeplerData.js
@@ -2,11 +2,23 @@
 import esb from 'elastic-builder';
 import client from '../elasticsearch/connection.js';
 import moment from 'moment';
+import _ from 'lodash';
 import fs from 'fs';
+import streamToPromise from 'stream-to-promise';
 import csv from 'fast-csv';
 var os = require("os");
 var dns = require("dns");
 const util = require('util');
+import regionsData from '../data/regions.json';
+import clustersData from '../data/clusters.json';
+
+const externalCameras = _.flatten(clustersData.clusters.map( cluster =>
+      cluster.cameras
+));
+
+const internalCameras = _.flatten(regionsData.regions.map( region => {
+  return region.cameras
+}));
 
 class KeplerData {
 
@@ -75,30 +87,50 @@ class KeplerData {
     const _from = moment(this.from, 'DD/MM/YYYY').format('DD_MM_YYYY');
     const _till = moment(this.till, 'DD/MM/YYYY').format('DD_MM_YYYY');
 
-    const fileName = `./data/snaps_${_from}_${_till}.csv`;
+    const fileName = `/data/snaps_${_from}_${_till}.csv`;
 
     var csvStream = csv.createWriteStream({headers: true}),
-        writableStream = fs.createWriteStream(fileName);
+        writableStream = fs.createWriteStream('.' + fileName);
 
-    writableStream.on("finish", function(){
-      console.log("DONE!");
-    });
+    streamToPromise(writableStream).then(function () {
+      // resolves undefined
+      console.log('Resolves');
+    })
+
+    // writableStream.on("finish", function(){
+    //   console.log("DONE!");
+    // });
 
     csvStream.pipe(writableStream);
 
     while( snaps.length - 2 )  {
       const snap  = snaps.shift()._source;
-      const snap2 = snaps.shift()._source;
+      const exCamera = externalCameras.find( camera => {
+        return camera.id === snap.cameraId
+      });
 
+      const snap2 = snaps.shift()._source;
       if( snap.lpr === snap2.lpr ) {
-        csvStream.write({
-                          first_camera_id: snap.cameraId,
-                          snap_datetime: snap.dateTime,
-                          second_camera_id: snap2.cameraId,
-                          snap2_datetime: snap2.dateTime,
-                          vehicle_type: snap.vehicleType,
-                          lpr: snap.lpr
-                        });
+
+        const inCamera = internalCameras.find( camera => {
+          return camera.id === snap2.cameraId
+        });
+
+        if( exCamera && inCamera ) {
+
+          csvStream.write({
+                            first_camera_id: snap.cameraId,
+                            snap_datetime: snap.dateTime,
+                            first_camera_lat: exCamera.location.lat,
+                            first_camera_lon: exCamera.location.lon,
+                            second_camera_id: snap2.cameraId,
+                            snap2_datetime: snap2.dateTime,
+                            second_camera_lat: inCamera.location.lat,
+                            second_camera_lon: inCamera.location.lon,
+                            vehicle_type: snap.vehicleType,
+                            lpr: snap.lpr
+                          });
+        }
       }
     }
 
@@ -107,9 +139,8 @@ class KeplerData {
     const hostname = os.hostname();
     const dnsResolve = util.promisify(dns.resolve);
     const records = await dnsResolve(hostname, 'A');
-    //console.log(records);
 
-    return `http://${records[0]}:4000/data/tlv-trips.csv`;
+    return `http://${records[0]}:4000${fileName}`;
   }
 
   async getUrl() {
